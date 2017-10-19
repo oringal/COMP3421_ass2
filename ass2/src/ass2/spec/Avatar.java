ec2659ae2e5fe3e17bbe12862e2dee96a92dcb6a
Gladys Chan <glad_c12@hotmail.com>
2017-10-19 16:45:46 +1100

    camera in first person

--- ass2/src/ass2/spec/Avatar.java
+++ ass2/src/ass2/spec/Avatar.java
@@ -9,24 +9,14 @@ import ass2.spec.Texture;
 public class Avatar {
 	// used lecture code week 8 TexCylindar to draw cylinder
 
-	private static final double WALKING_SPEED = 10;
-	private static final double ROTATE_SPEED = 5;
-	
-	private double angleX = 0;
-	private double angleY = 0;
-	private double angleZ = 3;
-	
-	private double moveX = 0;
-	private double moveY = 0;
+	private static final double WALKING_SPEED = 1;
+	private static final double AVARTAR_HEIGHT = 1.5;
+
 	private double moveZ = 0;
 
 	private static final int SLICES = 32;
 
 	private Camera camera;
-//	private Texture headTexture;
-//	private Texture bodyTexture;
-	
-//	private Texture myTextures[];
 
 	public Avatar(Camera camera) {
 		this.camera = camera;
@@ -35,29 +25,44 @@ public class Avatar {
 
 	public void draw(GL2 gl, Texture[] tex) {
 		//GLU glu = new GLU();
+		//gl.glDisable(GL2.GL_CULL_FACE);
 		camera.render(gl);
-//
-//		if (camera.isFirstPerson()) {
-//			return;
-//		}
+
+		if (camera.isFirstPerson()) {
+			return;
+		}
+		gl.glPushMatrix();
 		
-//		double[] cameraPosition = camera.getPosition();
-//		gl.glTranslated(cameraPosition[0], cameraPosition[1], cameraPosition[2]);
-//		gl.glRotated(camera.getAngle(), 0, 1, 0);
+		double[] cameraPosition = camera.getPosition();
+		// moves back and forth with camera
+		gl.glTranslated(cameraPosition[0], cameraPosition[1] + AVARTAR_HEIGHT , cameraPosition[2]);
+		// turns with camera
+		gl.glRotated(camera.getAngle(), 0, 1, 0);
 		
 		// Commands to turn the cylinder.
-		gl.glRotated(angleZ, 0.0, 0.0, 1.0);
-		gl.glRotated(angleY, 0.0, 1.0, 0.0);
-		gl.glRotated(angleX, 1.0, 0.0, 0.0);
+//		gl.glRotated(90, 0.0, 0.0, 1.0);
+//		gl.glRotated(90, 0.0, 1.0, 0.0);
+//		gl.glRotated(90, 1.0, 0.0, 0.0);
 		//gl.glTranslated(0, 0 , 3);
-		//gl.glTranslated(moveX, moveY, moveZ);
+		//gl.glTranslated(0, 0, moveZ);
+		//gl.glRotated(90, 1.0, 0.0, 0.0);
+		drawCylinder(gl, tex);
+		
+		gl.glPopMatrix();
 
+	}
+	
+	private void drawCylinder(GL2 gl, Texture[] tex) {
+		
 		double angleIncrement = (Math.PI * 2.0) / SLICES;
 		double zFront = -1;
-		double zBack = -3;
-
-		//Draw the top of the cylinder with the canTop.bmp texture
-
+		double zBack = -2;
+		
+		gl.glPushMatrix();
+		
+		
+		gl.glRotated(-90, 1, 0, 0);
+		
 		gl.glBindTexture(GL2.GL_TEXTURE_2D, tex[Game.AVATARHEAD].getTextureId());
 		gl.glBegin(GL2.GL_POLYGON);{
 
@@ -65,10 +70,12 @@ public class Avatar {
 			{
 				double angle0 = i*angleIncrement;
 
-				gl.glNormal3d(0.0, 0.0, 1);
+				gl.glNormal3d(0.0, 1.0, 0.0);
 				gl.glTexCoord2d(0.5+0.5*Math.cos(angle0),0.5+0.5*Math.sin(angle0));
-				gl.glVertex3d(Math.cos(angle0), Math.sin(angle0),zFront);
+				gl.glVertex3d(0.5*Math.cos(angle0), 0.5*Math.sin(angle0), zFront);
+				
 			}
+			
 		}gl.glEnd();
 
 		gl.glBindTexture(GL2.GL_TEXTURE_2D, tex[Game.AVATARBODY].getTextureId());
@@ -76,8 +83,8 @@ public class Avatar {
 			for(int i=0; i<= SLICES; i++){
 				double angle0 = i*angleIncrement;
 				double angle1 = (i+1)*angleIncrement;
-				double xPos0 = Math.cos(angle0);
-				double yPos0 = Math.sin(angle0);
+				double xPos0 = 0.5*Math.cos(angle0);
+				double yPos0 = 0.5*Math.sin(angle0);
 				double sCoord = 2.0/SLICES * i; //Or * 2 to repeat label
 
 
@@ -85,47 +92,46 @@ public class Avatar {
 				gl.glTexCoord2d(sCoord,1);
 				gl.glVertex3d(xPos0,yPos0,zFront);
 				gl.glTexCoord2d(sCoord,0);
-				gl.glVertex3d(xPos0,yPos0,zBack);	        	
+				gl.glVertex3d(xPos0,yPos0,zBack);	
+				
 
 			}
+			
 		}gl.glEnd();    
-
-		//Draw the bottom of the cylinder also with the canTop.bmp texture :)
-		//just for demonstration.
+		
+		gl.glBindTexture(GL2.GL_TEXTURE_2D, tex[Game.AVATARFEET].getTextureId());
 		gl.glBegin(GL2.GL_POLYGON);{
 
 			for(int i = 0; i < SLICES; i++)
 			{
 				double angle0 = -i*angleIncrement;
 
-				gl.glNormal3d(0.0, 0.0, -1);
+				gl.glNormal3d(0.0, -1.0, 0.0);
 
 				gl.glTexCoord2d(0.5+0.5*Math.cos(angle0),0.5+0.5*Math.sin(angle0));
-				gl.glVertex3d(Math.cos(angle0), Math.sin(angle0),zBack);
+				gl.glVertex3d(0.5*Math.cos(angle0), 0.5*Math.sin(angle0), zBack);
+				
 			}
+			
 		}gl.glEnd();
+		gl.glPopMatrix();
 	}
 	
 	public void moveForward() {
-		//moveX = (moveX + WALKING_SPEED);
-		angleZ = angleZ + ROTATE_SPEED;
+		moveZ = moveZ + WALKING_SPEED;
 		camera.moveForward();
 	}
 
 	public void moveBackward() {
-		//moveX = (moveX - WALKING_SPEED);
-		//angleZ = angleZ - ROTATE_SPEED;
-		
+		moveZ = moveZ - WALKING_SPEED;
 		camera.moveBackward();
 	}
 
 	public void turnLeft() {
-		//angleX = angleX - ROTATE_SPEED;
 		camera.turnLeft();
 	}
 
 	public void turnRight() {
-		//angleX = angleX + ROTATE_SPEED;
 		camera.turnRight();
 	}
 	
