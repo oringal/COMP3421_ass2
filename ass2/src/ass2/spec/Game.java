e1667117c0f0e021efc1b0931747f560832a00a5
Antheny Yu <oringalyu@hotmail.com>
2017-10-20 16:57:50 +1100

    changed how the textures are loaded

--- ass2/src/ass2/spec/Game.java
+++ ass2/src/ass2/spec/Game.java
@@ -38,6 +38,19 @@ public class Game extends JFrame implements GLEventListener, KeyListener{
 	private static boolean downKey;
 	private static boolean firstPerson;
 	
+	private final static String[] texFileName = {	"textures/grass.png",		//0
+													"textures/road.png",  		//1
+													"textures/minionBody.png", 	//2
+													"textures/minionHead.png", 	//3
+													"textures/minionFeet.png", 	//4
+													"textures/branch.png",  	//5
+													"textures/leaf.png", 		//6
+													"textures/sun.png"			//7
+												};
+	
+	private final static String texFileExt = "png";
+
+	
 	public final static int GRASS = 0;
 	public final static int ROAD = 1;
 	public final static int AVATARBODY = 2;
@@ -165,40 +178,14 @@ public class Game extends JFrame implements GLEventListener, KeyListener{
 	}
 	
 	private void loadTextures(GL2 gl) {
-		String grassFile = "textures/grass.png";
-		String grassExt = "png";
-		
-		String roadFile = "textures/road.png";
-		String roadExt = "png";
-
-		String avatarHeadFile = "textures/minionHead.png";
-		String avatarHeadExt = "png";
-		
-		String avatarBodyFile = "textures/minionBody.png";
-		String avatarBodyExt = "png";
-		
-		String avatarFeetFile = "textures/minionFeet.png";
-		String avatarFeetExt = "png";
-		
-		String branchFile = "textures/branch.png";
-		String branchExt = "png";
-		
-		String leafFile = "textures/leaf.png";
-		String leafExt = "png";
-		
-		String sunFile = "textures/sun.png";
-		String sunExt = "png";
-				
-		textures[GRASS] = new Texture(gl,grassFile,grassExt,true);
-		textures[ROAD] = new Texture(gl,roadFile,roadExt,true);
-		textures[AVATARHEAD] = new Texture(gl, avatarHeadFile, avatarHeadExt, true);
-		textures[AVATARBODY] = new Texture(gl, avatarBodyFile, avatarBodyExt, true);
-		textures[AVATARFEET] = new Texture(gl, avatarFeetFile, avatarFeetExt, true);
-		textures[BRANCH] = new Texture(gl, branchFile, branchExt, true);
-		textures[LEAF] = new Texture(gl, leafFile, leafExt, true);
-		textures[SUN] = new Texture(gl, sunFile, sunExt, true);
-
-
+		textures[GRASS] = new Texture(gl,texFileName[GRASS],texFileExt,true);
+		textures[ROAD] = new Texture(gl,texFileName[ROAD],texFileExt,true);
+		textures[AVATARHEAD] = new Texture(gl, texFileName[AVATARHEAD], texFileExt, true);
+		textures[AVATARBODY] = new Texture(gl, texFileName[AVATARBODY], texFileExt, true);
+		textures[AVATARFEET] = new Texture(gl, texFileName[AVATARFEET], texFileExt, true);
+		textures[BRANCH] = new Texture(gl, texFileName[BRANCH], texFileExt, true);
+		textures[LEAF] = new Texture(gl, texFileName[LEAF], texFileExt, true);
+		textures[SUN] = new Texture(gl, texFileName[SUN], texFileExt, true);
 	}
 
 	@Override
